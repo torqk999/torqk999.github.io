ebdfec2d540acb3b8ba95029b8cde546f0d07743
Clark Thomson <clark_thomson2001@yahoo.com>
2025-08-03 19:52:40 -0400

    Update render.js

--- render.js
+++ render.js
@@ -150,6 +150,8 @@ function main() {
     let angle = 0;
   
     function render() {
+        gl.viewport(0, 0, canvas.width, canvas.height);
+
         gl.clearColor(0, 0, 0, 1);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         gl.enable(gl.DEPTH_TEST);
